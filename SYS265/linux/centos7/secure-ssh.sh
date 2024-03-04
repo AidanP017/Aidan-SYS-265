576206118855efbbafa8d80c4a15d625eb808499
AidanP017 <aidan.pasek@mymail.champlain.edu>
2024-03-04 13:30:51 -0500

    edited .sh file to create a user named testuser with passwordless login

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,4 +3,9 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+sudo useradd -m -d /home/testuser -s /bin/bash testuser
+sudo mkdir /home/testuser/.ssh
+sudo cp /home/aidan/Aidan-SYS-265/SYS265/linux/public-keys/id_rsa.pub /home/testuser/.ssh/authorized_keys
+sudo chmod 700 /home/testuser/.ssh
+sudo chmod 600 /home/testuser/.ssh/authorized_keys
+sudo chown -R testuser:testuser /home/testuser/.ssh
